c905c59108ae09737cafbce77471ac5af2b290bf
K Samarth N Kamath <kamathsamarth@gmail.com>
2020-05-04 09:49:52 +0530

    Create Fashion_MNIST.py

---
+++ Project_4/Fashion_MNIST.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from os import path, getcwd, chdir
+path = f"{getcwd()}/../tmp2/mnist.npz"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+#end of configuration
+
+def train_mnist_conv():
+    # Please write your code only where you are indicated.
+    # please do not remove model fitting inline comments.
+
+    # YOUR CODE STARTS HERE
+    class myCallback(tf.keras.callbacks.Callback):
+      def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('acc')>0.998):
+          print("\nReached 99.8% accuracy so cancelling training!")
+          self.model.stop_training = True
+
+    # YOUR CODE ENDS HERE
+
+    mnist = tf.keras.datasets.mnist
+    (training_images, training_labels), (test_images, test_labels) = mnist.load_data(path=path)
+    
+    training_images=training_images.reshape(60000, 28, 28, 1)
+    training_images=training_images / 255.0
+    test_images = test_images.reshape(10000, 28, 28, 1)
+    test_images=test_images/255.0
+    
+    callbacks = myCallback()
+    
+
+    model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128,activation='relu'),
+            tf.keras.layers.Dense(10,activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # model fitting
+    model.summary()
+    history = model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
+    
+    return history.epoch, history.history['acc'][-1]
+
+_,_=train_mnist_conv()
