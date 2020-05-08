4e417a279a5d73d889cecb46cb643698eb88af96
K Samarth N Kamath <kamathsamarth@gmail.com>
2020-05-08 12:05:19 +0530

    Update Horse_Human.py

--- Project_5/Horse_Human.py
+++ Project_5/Horse_Human.py
@@ -1,3 +1,6 @@
+"""
+The following python code will use the OS library to use Operating System libraries, giving you access to the file system, and the zipfile library allowing you to unzip the data.
+"""
 import os
 import zipfile
 
@@ -5,3 +8,4 @@ local_zip = '/tmp/horse-or-human.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('/tmp/horse-or-human')
 zip_ref.close()
+
